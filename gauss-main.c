61eb811e6093d964895186fbf0b14b31c503cc5e
SternKomo <stern.komo@gmail.com>
2022-02-22 13:53:35 +0900

    ガウスの消去法を追加。TODO:解けない場合の処理を追加する

---
+++ gauss-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "numerical-calculus.h"
+
+int main(void){
+  int n = 3;
+  double A[n][n], x[n], b[n];
+  A[0][0]=1; A[0][1]=-1; A[0][2]=2;
+  A[1][0]=0; A[1][1]=1; A[1][2]=-2;
+  A[2][0]=2; A[2][1]=-2; A[2][2]=5;
+  b[0]=-8;
+  b[1]=5;
+  b[2]=-15;
+
+  gauss(n, A, x, b);
+
+  for(int i=0; i<n; i++)
+    printf("%lf\n", x[i]);
+
+  return 0;
+}
--- numerical-calculus.h
+++ numerical-calculus.h
@@ -1,5 +1,11 @@
 #pragma once
 /*
+ * ガウスの消去法 Ax = b
+ * input : n, A[n][n], x[n], b[n]
+ * output: x に格納
+ */
+void gauss(int n, double a[n][n], double*, double*);
+/*
  * ルンゲクッタ法
  * input: dim, x[dim], tmin, tmax, n, function
  */
